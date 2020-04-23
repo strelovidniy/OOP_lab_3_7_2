ae9e88b09876a015a2d9f3ea27116193cc1dca64
Роман Данилевич <dwordchanel@gmail.com>
2020-04-23 04:07:54 +0300

    Add project files.

---
+++ OOP_lab_3_7_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fileInput = new StreamReader("input.txt");
+
+            string word = fileInput.ReadLine();
+
+            fileInput.Close();
+
+            char[] newWord = word.ToCharArray();
+
+            newWord[0] = word[3];
+            newWord[3] = word[0];
+
+            StreamWriter fileOutput = new StreamWriter("output.txt", false);
+
+            fileOutput.WriteLine(newWord);
+
+            fileOutput.Close();
+        }
+    }
+}
